0b49048d80b2a99ba62977b7428b0c774ed36ab8
unknown <venkataharshini2812@gmail.com>
2025-11-09 15:27:07 +0530

    Initial Commit 2

--- resource-management/src/services/firebase.js
+++ resource-management/src/services/firebase.js
@@ -5,15 +5,7 @@ import { getStorage } from 'firebase/storage';
 
 // TODO: Replace with your Firebase project configuration
 // Get these values from Firebase Console > Project Settings > General
-const firebaseConfig = {
-  apiKey: "AIzaSyDf5DrGv9PuOyIg0uqpcE1Q09-iHAb1hrI",
-  authDomain: "eastman-eb9cf.firebaseapp.com",
-  projectId: "eastman-eb9cf",
-  storageBucket: "eastman-eb9cf.firebasestorage.app",
-  messagingSenderId: "850492769137",
-  appId: "1:850492769137:web:303f6ad420b350d65dacd0",
-  measurementId: "G-KSQLZFVY10"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
